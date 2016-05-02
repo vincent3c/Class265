330a9a834e6afa7187d2867416f65630eabbb158
Wuteyan <killerw74@gmail.com>
2016-05-02 21:01:18 +0800

    implement getData and add

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -3,6 +3,15 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class DrinkMenuActivity extends AppCompatActivity {
 
@@ -12,6 +21,64 @@ public class DrinkMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drink_menu);
         Log.d("debug", "DrinkMenuActivity OnCreate");
     }
+
+    public void add(View view)
+    {
+        Button button = (Button)view;
+
+        String text = button.getText().toString();
+
+        int count = Integer.parseInt(text);
+
+        count++;
+
+        button.setText(String.valueOf(count));
+    }
+
+    public void cancel(View view)
+    {
+        finish();
+    }
+
+    public void done(View view)
+    {
+
+    }
+
+    public JSONArray getData()
+    {
+        LinearLayout rootLinearLayout = (LinearLayout)findViewById(R.id.root);
+
+        JSONArray jsonArray = new JSONArray();
+
+        for(int i = 1; i < 4 ; i++)
+        {
+            LinearLayout linearLayout = (LinearLayout)rootLinearLayout.getChildAt(i);
+
+            TextView textView = (TextView)linearLayout.getChildAt(0);
+            Button mButton = (Button)linearLayout.getChildAt(1);
+            Button lButton = (Button)linearLayout.getChildAt(2);
+
+            String drinkName = textView.getText().toString();
+
+            int m = Integer.parseInt(mButton.getText().toString());
+            int l = Integer.parseInt(lButton.getText().toString());
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("name", drinkName);
+                jsonObject.put("m", m);
+                jsonObject.put("l", l);
+
+                jsonArray.put(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return jsonArray;
+    }
+
 
     @Override
     protected void onStart() {
